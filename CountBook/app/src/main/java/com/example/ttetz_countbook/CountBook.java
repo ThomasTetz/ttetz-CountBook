442721f0f964aa12415ba54d54690abd2d8fc39b
ThomasTetz <thomas.tetz@gmail.com>
2017-09-21 15:05:10 -0600

    Changed to empty activity with custom widgets

--- CountBook/app/src/main/java/com/example/ttetz_countbook/CountBook.java
+++ CountBook/app/src/main/java/com/example/ttetz_countbook/CountBook.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class CountBook extends AppCompatActivity {
 
     private TextView mTextMessage;
 
@@ -18,13 +18,15 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText("Add");
+//                    mTextMessage.setText(R.string.title_home); //title_home/ dashboard / notifications
+//                    "@string/title_home"
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText("Edit");
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText("Delete");
                     return true;
             }
             return false;
@@ -35,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
